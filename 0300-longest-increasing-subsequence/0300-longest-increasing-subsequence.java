dadd3c172edfdb74266fee2aadf5296214b46c1c
Divyanshu Kumar <143829106+kumarDivyanshu@users.noreply.github.com>
2025-08-07 23:45:29 +0530

    Time: 5 ms (92.87%), Space: 44.5 MB (48.62%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,12 +1,33 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int n = nums.length;
-        int prevIndex = -1;
-        int[][] dp = new int[n][n+1];
-        for(int [] arr: dp){
-            Arrays.fill(arr,-1);
+        // int n = nums.length;
+        // int prevIndex = -1;
+        // int[][] dp = new int[n][n+1];
+        // for(int [] arr: dp){
+        //     Arrays.fill(arr,-1);
+        // }
+        // return solve(n-1, prevIndex, nums, dp);
+
+        List<Integer> arr = new ArrayList<>();
+
+        for(int num  : nums)
+        {
+            int index = Collections.binarySearch(arr, num);
+
+            // If not found, binarySearch returns (-(insertion point) - 1)
+            if (index < 0) {
+                index = -index - 1;
+            }
+
+            if(index==arr.size()){
+                arr.add(num);
+            }
+            else{
+                arr.set(index , num);
+            }
         }
-        return solve(n-1, prevIndex, nums, dp);
+        return arr.size();
+
     }
 
     public int solve(int curr , int prev ,int[] nums, int [][]dp){
