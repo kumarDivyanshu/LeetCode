00a4d9f7738ea20412b1e164cd8ce3628e3cafe9
Divyanshu Kumar <143829106+kumarDivyanshu@users.noreply.github.com>
2025-08-14 11:27:37 +0530

    Time: 7 ms (56.91%), Space: 61.1 MB (70.39%) - LeetHub

---
+++ 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.java
@@ -0,0 +1,88 @@
+class DisjointSet {
+    int[] rank;
+    int[] parent;
+    int[] size;
+    public DisjointSet(int n) {
+        rank= new int[n+1];
+        parent = new int[n+1];
+        size = new int[n+1];
+
+        for(int i=0;i<=n;i++){
+            rank[i] = 1;
+            size[i] = 1;
+            parent[i] = i;
+        }
+    }
+
+    public int findUltParent(int node){
+        if(node == parent[node]){
+            return node;
+        }
+        return parent[node] = findUltParent(parent[node]);
+    }
+
+    public boolean find(int u, int v) {
+        int ult_u = findUltParent(u);
+        int ult_v = findUltParent(v);
+
+        return ult_u== ult_v; 
+        
+    }
+
+    public void unionByRank(int u, int v) {
+        int ult_u = findUltParent(u);
+        int ult_v = findUltParent(v);
+
+        if(ult_u == ult_v) return;
+        if(rank[ult_u] < rank[ult_v]){
+            parent[ult_u] = ult_v;
+        } 
+        else if(rank[ult_u] > rank[ult_v]){
+            parent[ult_v] = ult_u;
+        }
+        else{
+            parent[ult_u] = ult_v;
+            rank[ult_v]++;
+        }
+    }
+
+    public void unionBySize(int u, int v) {
+        int ult_u = findUltParent(u);
+        int ult_v = findUltParent(v);
+        if(ult_u == ult_v) return;
+
+        if(size[ult_u] < size[ult_v]){
+            parent[ult_u] = ult_v;
+            size[ult_v] += size[ult_u];
+        }
+        else {
+            parent[ult_v] = ult_u;
+            size[ult_u] += size[ult_v];
+        }
+    }
+}
+
+class Solution {
+    public int makeConnected(int n, int[][] connections) {
+        int extra = 0;
+        DisjointSet ds = new DisjointSet(n);
+        for(int i=0;i< connections.length ;i++){
+            int u = connections[i][0];
+            int v = connections[i][1];
+
+            if(ds.findUltParent(u) == ds.findUltParent(v)){
+                extra++;
+            }
+
+            ds.unionByRank(u,v);
+        }
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(ds.parent[i] == i){
+                count++;
+            }
+        }
+        int needed = count - 1; // operations needed to connect all components
+        return (extra >= needed) ? needed : -1;
+    }
+}
