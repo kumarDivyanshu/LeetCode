2223c6953399c82957dfc986eeb7ac2d713a70b5
Divyanshu Kumar <143829106+kumarDivyanshu@users.noreply.github.com>
2025-09-09 19:00:24 +0530

    Time: 746 ms (50.25%), Space: 0B (100%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -1,10 +1,18 @@
 # Write your MySQL query statement below
 
-select d.name Department, e.name Employee, e.salary Salary
-from Employee e join Department d
-on e.departmentId = d.id
-where e.salary =(select max(salary) from employee where departmentId= e.departmentId)
+-- select d.name Department, e.name Employee, e.salary Salary
+-- from Employee e join Department d
+-- on e.departmentId = d.id
+-- where e.salary =(select max(salary) from employee where departmentId= e.departmentId)
 
 -- select d.
 
 -- select departmentId ,max(salary) Salary from employee group by departmentId
+
+select d.name as Department,e.name as Employee,e.salary as Salary from 
+employee e 
+join 
+department d 
+on e.departmentId=d.id 
+where (e.departmentid,e.salary) in 
+(select departmentid,max(salary) from employee group by departmentId);
