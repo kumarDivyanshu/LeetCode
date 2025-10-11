912a86bec97aa4283d79c2c86ad5fbea59f97087
Divyanshu Kumar <143829106+kumarDivyanshu@users.noreply.github.com>
2025-10-11 10:15:55 +0530

    Time: 0 ms (100%), Space: 44.7 MB (97.77%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,26 +1,27 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         int org_color = image[sr][sc];
-        boolean[][] visited = new boolean[image.length+1][image[0].length+1];
-        dfs(image,sr,sc,color,org_color,visited);
+        if (org_color == color) return image;
+        // boolean[][] visited = new boolean[image.length+1][image[0].length+1];
+        dfs(image,sr,sc,color,org_color);
         return image;
     }
 
     int[] dr={-1,1,0,0};
     int[] dc={0,0,-1,1};
-    public void dfs(int[][] image, int sr, int sc, int color , int org_color, boolean[][] visited)
+    public void dfs(int[][] image, int sr, int sc, int color , int org_color)
     {
-        if(sr<0 || sc<0 || sr>=image.length || sc>=image[0].length || image[sr][sc]!=org_color || visited[sr][sc])
+        if(sr<0 || sc<0 || sr>=image.length || sc>=image[0].length || image[sr][sc]!=org_color )
         {
             return;
         }
         image[sr][sc] = color;
-        visited[sr][sc] = true;
+        // visited[sr][sc] = true;
         for(int i=0;i<4;i++)
         {
             int tr = sr + dr[i];
             int tc = sc + dc[i];
-            dfs(image,tr,tc, color , org_color, visited);
+            dfs(image,tr,tc, color , org_color);
             
         }
     }
