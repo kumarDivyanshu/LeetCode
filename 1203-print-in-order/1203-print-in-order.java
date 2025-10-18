7ec122956c417e59d9b2144dbc40d7d46c7b2a63
Divyanshu Kumar <143829106+kumarDivyanshu@users.noreply.github.com>
2025-10-19 00:08:29 +0530

    Time: 12 ms (52.09%), Space: 42.4 MB (49.22%) - LeetHub

--- 1203-print-in-order/1203-print-in-order.java
+++ 1203-print-in-order/1203-print-in-order.java
@@ -1,39 +1,31 @@
-class Foo {
-    volatile int counter = 1;
-    public Foo() {
-        
-        ExecutorService executor = Executors.newFixedThreadPool(3);
-        Runnable first = () -> System.out.println("first"); 
-        Runnable second = () -> System.out.println("second"); 
-        Runnable third = () -> System.out.println("third"); 
-        executor.execute(first);
-        executor.execute(second);
-        executor.execute(third);
-        executor.shutdown();
+import java.util.concurrent.Semaphore;
 
+class Foo {
+
+    private Semaphore secondSemaphore;
+    private Semaphore thirdSemaphore;
+
+    public Foo() {
+        secondSemaphore = new Semaphore(0); 
+        thirdSemaphore = new Semaphore(0);  
     }
 
     public void first(Runnable printFirst) throws InterruptedException {
-        
-        // printFirst.run() outputs "first". Do not change or remove this line.
+
         printFirst.run();
-        counter++;
+        secondSemaphore.release(); 
     }
 
     public void second(Runnable printSecond) throws InterruptedException {
-        while(counter != 2){
+        secondSemaphore.acquire();
 
-        }
-        // printSecond.run() outputs "second". Do not change or remove this line.
         printSecond.run();
-        counter++;
+        thirdSemaphore.release();
     }
 
     public void third(Runnable printThird) throws InterruptedException {
-        while(counter != 3){
-            
-        }
-        // printThird.run() outputs "third". Do not change or remove this line.
+        thirdSemaphore.acquire();
+
         printThird.run();
     }
 }
