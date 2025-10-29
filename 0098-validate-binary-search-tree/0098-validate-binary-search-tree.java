6f40a7b31f6ea89acdead9d2e191bb3ab5f6cbef
Divyanshu Kumar <143829106+kumarDivyanshu@users.noreply.github.com>
2025-10-29 16:59:18 +0530

    Time: 0 ms (100%), Space: 43.1 MB (89.67%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -20,14 +20,25 @@ class Solution {
         return ans;
     }
 
-    public boolean solve(TreeNode root , long min , long max){
-        if(root == null) return true;
+    // public boolean solve(TreeNode root , long min , long max){
+    //     if(root == null) return true;
 
+    //     if(root.val <= min || root.val >= max) return false;
+
+    //     boolean left  = solve(root.left, min , root.val);
+    //     boolean right = solve(root.right, root.val , max);
+
+    //     return left && right;
+    // }
+
+    public boolean solve(TreeNode root, long min , long max)
+    {
+        if(root == null ) return true;
         if(root.val <= min || root.val >= max) return false;
 
-        boolean left  = solve(root.left, min , root.val);
-        boolean right = solve(root.right, root.val , max);
+        boolean left = solve(root.left , min , root.val);
+        boolean right = solve(root.right , root.val , max);
 
-        return left && right;
+        return (left && right);
     }
 }
