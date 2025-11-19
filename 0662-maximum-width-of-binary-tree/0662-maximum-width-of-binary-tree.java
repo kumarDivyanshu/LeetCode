19e0571f680af454d0c361fcce3a24854dc635f4
Divyanshu Kumar <143829106+kumarDivyanshu@users.noreply.github.com>
2025-11-19 20:50:46 +0530

    Time: 1 ms (99.19%), Space: 45 MB (20.96%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
@@ -39,7 +39,7 @@ class Solution {
             for(int i=0;i< n;i++){
                 Pair curr = q.poll();
                 TreeNode node = curr.node;
-                int num = curr.num - mmin;
+                int num = curr.num;
 
                 if(i==0){
                     first = num;
