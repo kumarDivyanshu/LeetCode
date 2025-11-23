0b53409a4c3b23561567a68fa25f7105aef75828
Divyanshu Kumar <143829106+kumarDivyanshu@users.noreply.github.com>
2025-11-23 10:52:12 +0530

    Time: 107 ms (37.4%), Space: 47.2 MB (5.34%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,15 +1,23 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        return helper(nums, target, 0, 0);
+        Map<String, Integer> memo = new HashMap<>();
+        return helper(nums, target, 0, 0, memo);
     }
 
-    private int helper(int[] arr, int target, int idx, int sum) {
-        if (idx >= arr.length)
+    private int helper(int[] arr, int target, int idx, int sum, Map<String, Integer> memo) {
+        if (idx == arr.length) {
             return sum == target ? 1 : 0;
-        
-        int add = helper(arr, target, idx + 1, sum + arr[idx]);
-        int subtract = helper(arr, target, idx + 1, sum - arr[idx]);
+        }
 
-        return subtract + add;
+        String key = idx + "," + sum;
+        if (memo.containsKey(key)) {
+            return memo.get(key);
+        }
+
+        int add = helper(arr, target, idx + 1, sum + arr[idx], memo);
+        int subtract = helper(arr, target, idx + 1, sum - arr[idx], memo);
+
+        memo.put(key, add + subtract);
+        return add + subtract;
     }
 }
